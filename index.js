44c43ea933d6e2c415dd3a5a81a3f2326180ec6e
Bourgeois9792 <61666392+Bourgeois9792@users.noreply.github.com>
2020-04-19 23:25:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 var prefix = ('#')
 
-bot.login("MkWAhnNBlh-xs_U0f1_FwK8BBZp_9nxZ");
+bot.login(process.env.TOKEN);
 
 bot.on("ready", async () =>{
         console.log(" ")
